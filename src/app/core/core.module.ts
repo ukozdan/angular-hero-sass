818d9fbb380e77ac08ab2509a71443bd97d7d0d4
ukozdan <danroysanders@gmail.com>
2019-10-16 14:09:14 +0100

    1 file modified

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import {
+  FontAwesomeModule,
+  FaIconLibrary
+} from "@fortawesome/angular-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { UserService } from "./services/user.service";
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgbModule, FontAwesomeModule],
   providers: [UserService],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent]
